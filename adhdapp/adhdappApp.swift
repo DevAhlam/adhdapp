ea20b1eabe6517c68e40596fa666d01285b11a3a
DevAhlam <Ahlamalmutiri.dev@gmail.com>
2023-10-05 12:11:35 +0300

    Initial Commit

---
+++ adhdapp/adhdappApp.swift
@@ -0,0 +1,17 @@
+//
+//  adhdappApp.swift
+//  adhdapp
+//
+//  Created by Ahlamalmutiri on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct adhdappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
